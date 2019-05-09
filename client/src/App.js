d8f099cbbee3acf00f5402c285881e3bffd31b47
tblais1224 <thomasblais.student@careerdevs.com>
2019-05-09 16:44:49 -0400

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div className="App center">
+        <button className="red btn-center btn-floating btn-large scale-transition center">Submit</button>
       </div>
     );
   }
