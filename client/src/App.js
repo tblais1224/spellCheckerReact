fe8388d3715f1590ead3f34c97ae690e69a5f3e4
tblais1224 <thomasblais.student@careerdevs.com>
2019-05-10 16:29:29 -0400

    trying to send data from react to theserver

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 
 class App extends Component {
   state = {
-    header: ""
+    data: "",
+    userInput: ""
   }
-  getContent = () => {
-    fetch("/")
-    .then(res=> res.json())
+  
+  getDataFromDb = () => {
+    fetch("http://localhost:3001/api/getData")
+      .then(data => data.json())
+      .then(res => {
+        console.log(res.data)
+        this.setState({ data: res.data })
+      });
+  };
+
+  setInput = event => {
+    this.setState({
+      userInput: event.target.value
+    })
+
+    const headers = new Headers()
+    headers.append("Content-type", "application/json")
+
+    const options ={
+      method: 'POST',
+      headers,
+      body: JSON.stringify(event.target.value)
+    }
+
+    const request = new Request("http://localhost:3001/api/putData", options)
+    fetch(request)
+
+
+}
+
+  putDataToDB = () => {
+    axios.post("http://localhost:3001/api/putData", {
+      userInput: this.state.userInput
+    }).then(function(response){
+       console.log(response);
+       //Perform action based on response
+   })
+     .catch(function(error){
+       console.log(error);
+       //Perform action based on error
+     });
   }
+
+
   render() {
     return (
       <div className="App center">
-        <button className="red btn-center btn-floating btn-large scale-transition center">Submit</button>
+        <div className="center container">
+          <h1>Spell Check!</h1>
+          <textarea onChange={this.setInput}rows="15" cols="50" id="user-input"/>
+          <button onClick={this.putDataToDB} className="red btn-center btn-floating btn-large scale-transition center">Submit</button>
+        </div>
+        <div className="center container">
+          <p>{this.state.data}</p>
+          <p>{this.state.data}</p>
+        </div>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require("express")
 const app = express();
+var cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+app.use(cors());
+const router = express.Router();
 
-app.get("/", (req, res) => {
-    res.send({header:"<h1>THE test ROUTE IS WORKING</h1>"})
+router.get("/getData", (req, res) => {
+    res.send({data:"String after checked in database!"})
 })
 
-const PORT = process.env.PORT || 5000;
+var checkWords = [];
+router.get("/putData", (req,res) => {
+    var input = req.body
+    checkWords.push(res.data)
+    console.log(checkWords);
+    // console.log(input)
+    // return res.json({
+    //     success: "recieved",
+    //   });
+})
+
+app.use("/api", router);
+
+const PORT = 3001;
 app.listen(PORT, () => console.log("Server is running on port: "+ PORT))
+
+
+module.exports = router
